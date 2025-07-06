42d18d6d3e9ebdbd84580e46f52f95dea99a59a8
dandan-byte <75590778+dandan-byte@users.noreply.github.com>
2025-07-06 19:51:58 +0000

    Finish assignments 08 and 09.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    return array.filter(val => !theArgs.includes(val));
 };
 
 // Do not edit below this line
