4b2a0ac543c2c2979178bd5afce096d0d60c6915
dandan-byte <75590778+dandan-byte@users.noreply.github.com>
2025-07-06 20:05:43 +0000

    Finish assignment 11.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  const cTemp = ((fTemp - 32) * (5/9));
+  const cTempStr = cTemp.toFixed(1);
+  return parseFloat(cTempStr);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const fTemp = (cTemp * (9/5) + 32);
+  const fTempStr = fTemp.toFixed(1);
+  return parseFloat(fTempStr);
 };
 
 // Do not edit below this line
