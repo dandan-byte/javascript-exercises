fa23da48399936e109bac4378bcb3f40418f11ab
dandan-byte <75590778+dandan-byte@users.noreply.github.com>
2025-07-06 17:24:38 +0000

    Finish assignment 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(str, num) {
+    let newString = "";
+    for (let i = 0; i < num; i++){
+        newString += str;
+    };
+    return newString;
+}
 // Do not edit below this line
 module.exports = repeatString;
